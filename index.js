d3fdc489105fb62014347216a4c4e039f1a80378
Luis Martinez <luis_mmartinez@live.com>
2021-06-04 23:28:09 -0400

    commit tree 4fdce4e8be129aa099a7047bf1db525b3f1f138d

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+class Polygon {
+  constructor(array){
+    this.array = array;
+  }
+
+  get countSides(){
+    return this.array.length;
+  }
+
+  get perimeter(){
+    return this.array.reduce((accumulator, currentValue) => accumulator + currentValue)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    let side1 = this.array[0];
+    let side2 = this.array[1];
+    let side3 = this.array[2];
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid(){
+    let side1 = this.array[0];
+    let side2 = this.array[1];
+    let side3 = this.array[2];
+    let side4 = this.array[3];
+    return side1 === side2 && side1 === side4 && side2 && side3 ? true : false;
+  }
+
+  get area(){
+    return (this.array[0])**2;
+  }
+}
+
